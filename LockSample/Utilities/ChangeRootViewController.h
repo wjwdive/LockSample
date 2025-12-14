e96f5bb725cbb1f92f3f2d96e43a6cbe75e79e7e
wjwdive <wjwdive@163.com>
2025-12-14 22:37:20 +0800

    信号量+帧动画

---
+++ LockSample/Utilities/ChangeRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChangeRootViewController.h
+//  LockSample
+//
+//  Created by wjw on 2025/12/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChangeRootViewController : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LockSample/Utilities/ChangeRootViewController.m
@@ -0,0 +1,12 @@
+//
+//  ChangeRootViewController.m
+//  LockSample
+//
+//  Created by wjw on 2025/12/14.
+//
+
+#import "ChangeRootViewController.h"
+
+@implementation ChangeRootViewController
+
+@end
