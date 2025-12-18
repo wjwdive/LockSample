008ba252a39ab979fe49f3192bbbfcc2c56eef98
wjwdive <wjwdive@163.com>
2025-12-18 17:03:16 +0800

    gcd同步依赖，异步依赖（错误示例）

--- LockSample/GCD/TaskScheduler.h
+++ LockSample/GCD/TaskScheduler.h
@@ -7,6 +7,12 @@
 
 /**
  使用信号量 控制任务的依赖/同步
+ 任务A先执行，任务B依赖任务A执行完成，任务C依赖任务B执行完成。
+ 用一个信号量初始化为0，
+ 开始执行任务A,任务A执行完成发送signal信号；
+ 异步提交任务B,开始执行任务B之前，先对信号量wait操作。然后执行B的具体任务代码。B任务执行完成之后，对信号量signal操作。
+ 同样，异步提交任务C，开始执行任务C之前，先对信号量wait操作。然后执行任务C的具体代码。任务C执行完成之后，对信号量signal操作。
+  
  */
 
 #import <Foundation/Foundation.h>
@@ -15,6 +21,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface TaskScheduler : NSObject
 - (void)executeTasksWithDependencies;
+- (void)executeTasksWithDependenciesSync;
 @end
 
 NS_ASSUME_NONNULL_END
--- LockSample/GCD/TaskScheduler.m
+++ LockSample/GCD/TaskScheduler.m
@@ -8,6 +8,8 @@
 #import "TaskScheduler.h"
 
 @implementation TaskScheduler
+
+// 异步执行任务链， 实际调试可能会发生优先级反转， 因为任务B和任务C都是异步执行的， 所以任务B和任务C的执行顺序是不确定的， 但是任务A一定是先执行的
 - (void)executeTasksWithDependencies {
     dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
     
@@ -43,4 +45,42 @@
         NSLog(@"任务C执行完成");
     });
 }
+
+// 同步执行任务链，确保按顺序执行
+- (void)executeTasksWithDependenciesSync {
+//    dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+    
+    //任务链开始
+    NSLog(@"开始执行任务链");
+    //任务A （后台执行）
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSLog(@"任务A开始执行");
+        [NSThread sleepForTimeInterval:1.0];//模拟耗时操作
+        NSLog(@"任务A执行完成");
+        //通知任务A执行完成
+//        dispatch_semaphore_signal(semaphore);
+    });
+    
+    // 任务B（等待任务A完成执行）
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        //等待任务A完成
+//        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"任务B开始执行(依赖任务A)");
+        [NSThread sleepForTimeInterval:0.5];
+        NSLog(@"任务B执行完成");
+        
+        //可以继续通知其他任务
+//        dispatch_semaphore_signal(semaphore);
+    });
+    
+    // 任务C（同样等待任务A完成执行）
+    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        //需要重新等待，因为信号量 已经被任务B使用
+//        dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+        NSLog(@"任务C开始执行(也依赖任务A)");
+        [NSThread sleepForTimeInterval:0.5];
+        NSLog(@"任务C执行完成");
+    });
+}
+
 @end
